6e055430f6a72b76448439d9ab2f744d8dfe25ca
Jeff Swisher <jefftswisher@gmail.com>
2019-12-19 18:23:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,5 +69,18 @@ class Dog
     result = DB[:conn].execute(sql, id)[0]
     Dog.new(id: result[0], name: result[1], breed: result[2])
   end 
+  
+  def self.find_or_create_by_name(name)
+    
+    
+  end 
+    
+    
+    
+    
+    
+    
+    
+    
     
 end 
